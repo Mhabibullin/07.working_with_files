af3233e5f94423b775e11286e9ef9856753f817e
IvanBo <aldeano3@gmail.com>
2021-08-23 01:39:25 +0300

    added test read excel file from path resources

---
+++ src/test/java/ReadExcelFile.java
@@ -0,0 +1,29 @@
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadExcelFile {
+
+    @Test
+    void readXlsx() throws IOException {
+        File fileToBeRead = new File("./src/test/resources/Invoice.xlsx");
+        FileInputStream fis = new FileInputStream(fileToBeRead);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet firstSheet = workbook.getSheetAt(0);
+        String getCellValue =
+                firstSheet
+                .getRow(2)
+                .getCell(1)
+                .getStringCellValue();
+
+        System.out.println("Print cell:");
+        System.out.println(getCellValue);
+        Assertions.assertEquals("Элемент 1", getCellValue);
+    }
+}
