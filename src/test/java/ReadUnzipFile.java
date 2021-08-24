3ebd7ae1695d302a24159dbc04923c950b0de253
IvanBo <aldeano3@gmail.com>
2021-08-24 23:50:45 +0300

    added ReadUnzipFile

---
+++ src/test/java/ReadUnzipFile.java
@@ -0,0 +1,42 @@
+import net.lingala.zip4j.ZipFile;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.apache.commons.io.FileUtils.cleanDirectory;
+
+public class ReadUnzipFile {
+
+    @AfterAll
+    static void clearUnzipDir() throws IOException {
+        cleanDirectory(new File("./src/test/resources/unzip"));
+    }
+
+    @Test
+    void readUnzipFile() throws IOException {
+        String zipFilePath = "./src/test/resources/cv.zip";
+        String unzipFolder = "./src/test/resources/unzip";
+        String zipPassword = "1234";
+        String unzipFileTxtPath = "./src/test/resources/unzip/cv.txt";
+        String expectedData = "Менеджер проекта";
+
+        ZipFile zipFile = new ZipFile(zipFilePath);
+        if (zipFile.isEncrypted()) {
+            zipFile.setPassword(zipPassword.toCharArray());
+        }
+        zipFile.extractAll(unzipFolder);
+
+        FileInputStream fis = new FileInputStream(unzipFileTxtPath);
+        String document = IOUtils.toString(fis, StandardCharsets.UTF_8);
+        Assertions.assertTrue(document.contains(expectedData));
+        fis.close();
+    }
+}
